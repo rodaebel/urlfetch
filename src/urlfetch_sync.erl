92bdf2fd30cd6348f8d7211dcf24b0b22d3b22a4
Tobias <Tobias@localhost>
2010-12-19 03:00:02 +0100

    Removed obsolete module.

--- src/urlfetch_sync.erl
+++
@@ -1,9 +0,0 @@
--module(urlfetch_sync).
-
--export([fetch/1]).
-
-
-fetch({Method, Url}) ->
-    {ok, {_Status, Head, Body}} = httpc:request(Method, {Url, []}, [], []),
-    Length = list_to_binary(proplists:get_value("content-length", Head, "0")),
-    {ok, Length, Body}.
